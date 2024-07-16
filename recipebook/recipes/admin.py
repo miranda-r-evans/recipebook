bd024756b0f387a8e0357e149a43dfebcb746e00
Miranda Evans <miranda.r.evans@gmail.com>
2024-07-16 14:43:51 -0700

    add admin configuration

--- recipebook/recipes/admin.py
+++ recipebook/recipes/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Recipe)
+admin.site.register(Comment)
+admin.site.register(Ingredient)
+admin.site.register(RecipeIngredient)
+admin.site.register(Rating)
